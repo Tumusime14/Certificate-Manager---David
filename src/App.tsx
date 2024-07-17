98cb1a3c1e71192e2e8e641f215f534526d885a3
tumusime_david <tumusimedavid14@gmail.com>
2024-07-17 15:50:03 +0200

    Some changes on config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import React, { FC } from 'react';
+import Sidebar from '../components/Sidebar/Sidebar';
 import './App.css';
-import Sidebar from './components/Sidebar/Sidebar';
 
 const App: FC = () => {
   return (
