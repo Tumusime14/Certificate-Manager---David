bb19d4f41f8da7dc877a3d704a9d008bbad399c2
tumusime_david <tumusimedavid14@gmail.com>
2024-07-18 18:08:02 +0200

    Chore/Task-1-KAN-8 Fixing endline on .prettierec

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,9 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-<<<<<<< HEAD
-  <StrictMode>
-    <App />
-  </StrictMode>
-=======
   <React.StrictMode>
     <App />
   </React.StrictMode>
->>>>>>> 2dc3d3b (Feature/Task-1-KAN-8 Added menu items, dropdown effect)
 );
 
 
