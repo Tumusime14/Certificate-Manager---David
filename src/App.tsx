f54acac673f13f08b5251f7284296a9fb2544367
tumusime_david <tumusimedavid14@gmail.com>
2024-07-23 00:27:47 +0200

    Feature/Task-3-KAN-46 Added state to store submitted certificate data and display them after being submitted.

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,37 @@ const App: FC = () => {
   const [title, setTitle] = useState<string>("Start");
   const [showTable, setShowTable] = useState<boolean>(false);
   const [showNewCertificate, setShowNewCertificate] = useState<boolean>(false);
+  const [tableData, setTableData] = useState<any[]>([
+    {
+      supplier: 'DAIMLER AG, 1, Berlin',
+      certificateType: 'Permission of Printing',
+      validFrom: '21.08.2017',
+      validTo: '26.08.2017'
+    },
+    {
+      supplier: 'ANDEMIS GmbH, 1, Stuttgart',
+      certificateType: 'OHSAS 18001',
+      validFrom: '18.08.2017',
+      validTo: '24.08.2017'
+    },
+    {
+      supplier: 'ANDEMIS GmbH, 1, Stuttgart',
+      certificateType: 'Permission of Printing',
+      validFrom: '04.10.2017',
+      validTo: '10.10.2017'
+    }
+  ]);
 
   const handleNewCertificate = () => {
     setShowTable(false);
     setShowNewCertificate(true);
     setTitle("");
+  };
+
+  const handleSaveData = (newData: any) => {
+    setTableData((prevData) => [...prevData, newData]); 
+    setShowNewCertificate(false);
+    setShowTable(true);
   };
 
   return (
