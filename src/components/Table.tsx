4fc7ee48218b259acd13207206bd6b52136551b4
tumusime_david <tumusimedavid14@gmail.com>
2024-07-23 09:58:20 +0200

    Feature/Task-4-KAN-54 Added static gear icon to table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import "../styles/Table.css";
-
+import "./Table.css";
+import GearIcon from './icons/gear';
 
 interface TableProps {
   onNewCertificate?: () => void;
@@ -13,7 +13,28 @@ const Table: React.FC<TableProps> = ({ onNewCertificate }) => {
       <button className="new-certificate-button" onClick={onNewCertificate}>
         New Certificate
       </button>
-
+      <table>
+        <thead>
+          <tr>
+            <td></td>
+            <td>Supplier</td>
+            <td>Certificate type</td>
+            <td>Valid from</td>
+            <td>Valid to</td>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              <td><GearIcon /></td>
+              <td>{row.supplier}</td>
+              <td>{row.certificateType}</td>
+              <td>{row.validFrom}</td>
+              <td>{row.validTo}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
