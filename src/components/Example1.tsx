683fe20b1c2012a0153ba91311603ce518162776
tumusime_david <tumusimedavid14@gmail.com>
2024-07-24 11:01:15 +0200

    Feature/Task-1-KAN-24 Implemented components(files Examples instaed of constants

---
+++ src/components/Example1.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Example1 = () =>{
+    return(
+        <h1>Example 1</h1>
+    )
+}
+
+export default Example1
---
+++ src/components/Example3.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Example3 = () =>{
+    return(
+        <h1>Example 3</h1>
+    )
+}
+
+export default Example3
