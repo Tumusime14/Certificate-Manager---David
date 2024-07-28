0bdc0d0c1dc7dfc30f0fe39073028ae862208e1d
tumusime_david <tumusimedavid14@gmail.com>
2024-07-29 00:08:54 +0200

    Feature/Task-6-KAN-64 Supplier lookup implemented successfully

--- src/components/certificates/CertificateForm.tsx
+++ src/components/certificates/CertificateForm.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import {  addCertificate, updateCertificate } from "../../DB/indexedDB";
+import { addCertificate, updateCertificate } from "../../DB/indexedDB";
 import { useNavigate } from 'react-router';
 import "../../styles/NewCertificate.css";
 import Search from '../../icons/search';
 import X from '../../icons/x';
 import { getCertificates } from "../../DB/indexedDB";
+import SupplierLookupModal from '../SupplierLookupModal';
 
 interface ICertificateForm {
   isEdit?: boolean
@@ -24,35 +25,27 @@ const CertificateForm: React.FC<ICertificateForm> = ({isEdit, certificateId}:ICe
     pdfPreview: '' as string | null,
   });
   const [error, setError] = useState<string | null>(null);
-  // const [isModalOpen, setisOpenModal] = useState(false);
-  // const openModal = () => {
-  //   setisOpenModal(true)
-  // }
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(()=> {
     if(isEdit && certificateId){
       async function fetchData() {
         const certificates = await getCertificates();
-
-const filteredCertificate = certificates.filter((certificate) => certificate.id ===certificateId)
-
-filteredCertificate.map((certificate)=> (
-
-
-  setFormData({
-    validFrom: certificate.validFrom ? certificate.validFrom : null,
-    validTo: certificate.validTo ? certificate.validTo : null,
-    certificateType: certificate.certificateType,
-    supplier: certificate.supplier,
-    pdfFile: certificate.pdfFile || null,
-    pdfPreview: certificate.pdfPreview || null
-  })
-))}
-  
+        const filteredCertificate = certificates.filter((certificate) => certificate.id === certificateId);
+        filteredCertificate.map((certificate) => (
+          setFormData({
+            validFrom: certificate.validFrom ? certificate.validFrom : '',
+            validTo: certificate.validTo ? certificate.validTo : '',
+            certificateType: certificate.certificateType,
+            supplier: certificate.supplier,
+            pdfFile: certificate.pdfFile || null,
+            pdfPreview: certificate.pdfPreview || null
+          })
+        ));
+      }
       fetchData();
     }
-
-  }, [certificateId])
+  }, [certificateId]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -67,16 +60,17 @@ filteredCertificate.map((certificate)=> (
       [e.target.name]: e.target.value,
     });
   };
+
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0] || null;
     if (file && file.type === 'application/pdf') {
       const reader = new FileReader();
       reader.onload = () => {
         if (reader.result) {
-                setFormData({
-        ...formData,
-        pdfPreview: reader.result as string,
-      });
+          setFormData({
+            ...formData,
+            pdfPreview: reader.result as string,
+          });
         }
       };
       reader.readAsDataURL(file);
@@ -84,7 +78,7 @@ filteredCertificate.map((certificate)=> (
       alert('Please upload a valid file.');
     }
   };
-  
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (!formData.pdfPreview) {
@@ -100,7 +94,7 @@ filteredCertificate.map((certificate)=> (
           validFrom: formData.validFrom,
           validTo: formData.validTo,
           pdfFile: formData.pdfFile,
-        }, certificateId as number)
+        }, certificateId as number);
       }
       else{
         await addCertificate({
@@ -111,7 +105,6 @@ filteredCertificate.map((certificate)=> (
           pdfFile: formData.pdfFile,
         });
       }
-      
       navigate('/example1');
       handleReset();
     } catch (error) {
@@ -131,87 +124,98 @@ filteredCertificate.map((certificate)=> (
     setError(null); 
   };
 
+  const handleSelectSupplier = (supplier: string) => {
+    setFormData({
+      ...formData,
+      supplier: supplier,
+    });
+    setIsModalOpen(false);
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="new-certificate-form">
-      <div className="form-left">
-        <div className="form-group">
-          <label>Supplier</label>
-          <div className="input-container">
-            <input 
-              type="text" 
-              name="supplier" 
-              value={formData.supplier} 
-              onChange={handleChange} 
-              required 
-              className="input-field" 
+    <>
+      <form onSubmit={handleSubmit} className="new-certificate-form">
+        <div className="form-left">
+          <div className="form-group">
+            <label>Supplier</label>
+            <div className="input-container">
+              <input 
+                type="text" readOnly
+                name="supplier" 
+                value={formData.supplier} 
+                // onChange={handleChange} 
+                required 
+                className="input-field" 
+              />
+              <Search className="icon" onClick={() => setIsModalOpen(true)} />
+              <X className="icon" onClick={() => setFormData({ ...formData, supplier: '' })} />
+            </div>
+          </div>
+          
+          <div className="form-group">
+            <label htmlFor="certificateType">Certificate Type</label>
+            <select name="certificateType" value={formData.certificateType} onChange={handleChanges} required>
+              <option value="">Select your option</option>
+              <option value="Permission of printing">Permission of printing</option>
+              <option value="OHSAS 18001">OHSAS 18001</option>
+              <option value="Attendance">Attendance</option>
+              <option value="Completion">Completion</option>
+            </select>
+          </div>
+          
+          <div className="form-group">
+            <label>Valid from</label>
+            <input
+              type="text" required
+              placeholder="Click to select date"
+              ref={validFromRef}
+              name="validFrom"
+              value={formData.validFrom}
+              onChange={handleChange}
+              onFocus={() => { validFromRef.current!.type = "date"; }}
             />
-            <Search className="icon"/>
-            <X className="icon" onClick={() => setFormData({ ...formData, supplier: '' })} />
+          </div>
+          
+          <div className="form-group">
+            <label>Valid to</label>
+            <input
+              type="text"
+              placeholder="Click to select date"
+              ref={validToRef}
+              name="validTo"
+              value={formData.validTo}
+              onChange={handleChange}
+              onFocus={() => { validToRef.current!.type = "date"; }}
+              required
+            />
+          </div>
+          
+          {error && <p style={{ color: 'red' }}>{error}</p>} 
+        </div>
+        
+        <div className="form-right">
+          <div className="form-group">
+            <input type="file" name="pdfFile" accept="application/pdf" onChange={handleFileChange} required style={{ display: 'none' }} />
+            <button type="button" className="upload-button" onClick={() => {
+              const fileInput = document.querySelector('input[name="pdfFile"]') as HTMLInputElement;
+              fileInput?.click();
+            }}>Upload</button>
+          </div>
+          <div className="pdf-preview-container">
+            {formData.pdfPreview ? (
+              <iframe src={formData.pdfPreview} title="PDF Preview" className="pdf-preview" />
+            ) : (
+              <div className="pdf-placeholder"></div>
+            )}
+          </div>
+          <div className="form-actions">
+            <button type="submit">Save</button>
+            <button type="button" onClick={handleReset}>Reset</button>
           </div>
         </div>
-        
-        <div className="form-group">
-          <label htmlFor="certificateType">Certificate Type</label>
-          <select name="certificateType" value={formData.certificateType} onChange={handleChanges} required>
-            <option value="">Select your option</option>
-            <option value="Permission of printing">Permission of printing</option>
-            <option value="OHSAS 18001">OHSAS 18001</option>
-            <option value="Attendance">Attendance</option>
-            <option value="Completion">Completion</option>
-          </select>
-        </div>
-        
-        <div className="form-group">
-          <label>Valid from</label>
-          <input
-            type="text" required
-            placeholder="Click to select date"
-            ref={validFromRef}
-            name="validFrom"
-            value={formData.validFrom}
-            onChange={handleChange}
-            onFocus={() => { validFromRef.current!.type = "date"; }}
-          />
-        </div>
-        
-        <div className="form-group">
-          <label>Valid to</label>
-          <input
-            type="text"
-            placeholder="Click to select date"
-            ref={validToRef}
-            name="validTo"
-            value={formData.validTo}
-            onChange={handleChange}
-            onFocus={() => { validToRef.current!.type = "date"; }}
-            required
-          />
-        </div>
-        
-        {error && <p style={{ color: 'red' }}>{error}</p>} 
-      </div>
-      
-      <div className="form-right">
-        <div className="form-group">
-          <input type="file" name="pdfFile" accept="application/pdf" onChange={handleFileChange} required style={{ display: 'none' }} />
-          <button type="button" className="upload-button" onClick={() => {
-            const fileInput = document.querySelector('input[name="pdfFile"]') as HTMLInputElement;
-            fileInput?.click();
-          }}>Upload</button>
-        </div>
-        <div className="pdf-preview-container">
-          {formData.pdfPreview ? (
-            <iframe src={formData.pdfPreview} title="PDF Preview" className="pdf-preview" />
-          ) : (
-            <div className="pdf-placeholder"></div>
-          )}
-        </div>
-        <div className="form-actions">
-          <button type="submit">Save</button>
-          <button type="button" onClick={handleReset}>Reset</button>
-        </div>
-      </div>
-    </form>
+      </form>
+      {isModalOpen && <SupplierLookupModal onClose={() => setIsModalOpen(false)} onSelect={handleSelectSupplier} />}
+    </>
   );
 };
 
